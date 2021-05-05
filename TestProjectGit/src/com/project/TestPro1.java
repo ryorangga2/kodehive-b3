1bf4b2bedad873ac339c92d8dbcbd42fdb9ccd6a
ryorangga.suma@gmail.com <ryorangga.suma@gmail.com>
2021-05-05 14:11:16 +0700

    git-pro-1

---
+++ TestProjectGit/src/com/project/TestPro1.java
@@ -0,0 +1,17 @@
+package com.project;
+
+public class TestPro1 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] arrays = {"apel, jeruk, mangga"};
+		
+		for (int i = 0; i < arrays.length; i++) {
+			System.out.println(arrays[i]);
+		}
+		
+	}
+
+}
